a23d460dc79dd421cbdd544f86fd6b2d5f5551bd
Roberto Sartori <roberto.sartori.android@gmail.com>
2021-10-08 23:57:27 +0200

    msm8998-common: disable compressed APEX
    Compressed APEX has been introduced in Android 12, and they are enabled
    by default inheriting updatable_apex.mk.
    Unfortunately, when the device is encrypted, they are causing bootloops
    on msm8998.
    As the only advantage is some space gain on disk, we can
    simply disable them: keep the variable immediatly after including the
    updatable_apex.mk to avoid being overwritten.
    
    Signed-off-by: Roberto Sartori <roberto.sartori.android@gmail.com>

--- msm8998.mk
+++ msm8998.mk
@@ -18,6 +18,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk
 
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+# Disable APEX compression
+# Keep this after including updatable_apex.mk
+PRODUCT_COMPRESSED_APEX := false
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
