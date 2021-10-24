edc694ec1552b1f864a9eac1a9bdbf58c2b9d02c
sbwml <984419930@qq.com>
2021-10-25 07:22:55 +0800

    Revert "Revert "msm8998-common: vndk: Enable runtime isolation""
    This reverts commit e6224f54aeadeb0d8b20a1ed162b2c76b957c1d8.

--- msm8998.mk
+++ msm8998.mk
@@ -418,6 +418,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.1-service.msm8998
 
+# VNDK
+PRODUCT_PACKAGES += \
+    libdng_sdk.vendor_32 \
+    libstdc++.vendor_32 \
+    vndk-ext
+
 # Wifi
 PRODUCT_PACKAGES += \
     ipacm \
