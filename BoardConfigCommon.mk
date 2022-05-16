81461537f23623c8dc5d49fc3a763d2a1524a136
Nanhumly <1607616422@qq.com>
2022-05-16 12:21:09 +0000

    msm8998-common: Remove temp sepolicy

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -145,7 +145,6 @@ TARGET_USES_OLD_MNC_FORMAT := true
 VENDOR_SECURITY_PATCH := 2020-01-01
 
 # SELinux
-BOARD_VENDOR_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy/temp
 #include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 #BOARD_VENDOR_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy/vendor
 #BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(PLATFORM_PATH)/sepolicy/public
