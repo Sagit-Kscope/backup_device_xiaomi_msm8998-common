c3d3ae34c6e6000191f7e71bfeeece0ff8c5fab2
Nanhumly <1607616422@qq.com>
2022-06-20 12:01:10 +0000

    msm8998-common: Fix Vulkan

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -133,6 +133,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.renderengine.backend=threaded \
     debug.sf.enable_hwc_vds=1 \
     debug.sf.hw=1 \
+    ro.hardware.egl=adreno \
+    ro.hardware.vulkan=adreno \
     ro.opengles.version=196610 \
     vendor.display.disable_partial_split=1 \
     vendor.display.disable_rotator_downscale=1 \
@@ -233,6 +235,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # SurfaceFlinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.max_virtual_display_dimension=4096 \
     ro.surface_flinger.protected_contents=true \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
