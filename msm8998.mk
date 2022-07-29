3d78d183226b4faf64da4e57ad7405f450fdf70b
Nanhumly <1607616422@qq.com>
2022-07-29 02:13:14 +0000

    Revert "Revert "msm8998-common: Introduce charge control HAL""
    This reverts commit 7a3b57dddb85abc669a268e551f09f4d146fa832.

--- msm8998.mk
+++ msm8998.mk
@@ -178,6 +178,10 @@ PRODUCT_PACKAGES += \
     libcnefeatureconfig \
     services-ext
 
+# Charge control
+PRODUCT_PACKAGES += \
+    vendor.kscope.chgctrl@1.0-service.xiaomi_msm8998
+    
 # Display
 PRODUCT_PACKAGES += \
     gralloc.msm8998 \
