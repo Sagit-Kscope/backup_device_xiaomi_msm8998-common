ac05207e10327ffc8f796542e3563c879ae21dae
Roberto Sartori <roberto.sartori.android@gmail.com>
2022-08-19 03:54:17 +0000

    msm8998-common: gps: Fix build error in AOSP Signed-off-by: Roberto Sartori <roberto.sartori.android@gmail.com>

--- gps/utils/LocTimer.cpp
+++ gps/utils/LocTimer.cpp
@@ -426,6 +426,19 @@ void LocTimerPollTask::destroy() {
     }
 }
 
+typedef union epoll_data
+{
+  void *ptr;
+  int fd;
+  uint32_t u32;
+  uint64_t u64;
+} epoll_data_t;
+
+struct epoll_event {
+               uint32_t     events;      /* Epoll events */
+               epoll_data_t data;        /* User data variable */
+};
+
 void LocTimerPollTask::addPoll(LocTimerContainer& timerContainer) {
     struct epoll_event ev;
     memset(&ev, 0, sizeof(ev));
