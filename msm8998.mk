cbcf991d2a9f9a7586f6d16eeba57b780b894cdf
Bruno Martins <bgcngm@gmail.com>
2022-09-14 14:51:15 +0100

    msm8998-common: Migrate to AIDL ClearKey DRM HAL
    Co-Authored-By: Huexxx <huexxx@gmail.com>
    Change-Id: Iad5cc1176978137c647069197b33f49fb4d98ab2

--- msm8998.mk
+++ msm8998.mk
@@ -215,7 +215,8 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm@1.3.vendor \
+    android.hardware.drm-service.clearkey
 
 # Dex/ART optimization
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
