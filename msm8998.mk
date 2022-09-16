dd02795626789686894613fcea3e8157cfd3ecd9
Nanhumly <1607616422@qq.com>
2022-09-16 15:18:40 +0000

    Revert "msm8998-common: Switch BtAudio to AIDL"
    This reverts commit 9bf38be0c14b6f799d95ea3c01b169fe6604b5ed.

--- msm8998.mk
+++ msm8998.mk
@@ -107,7 +107,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
-    android.hardware.bluetooth.audio-impl \
+    android.hardware.bluetooth.audio@2.0-impl \
     libbthost_if
 
 PRODUCT_COPY_FILES += \
