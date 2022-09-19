2ee1381f4a070fea73cd9be748e142e3e5c5bfb9
Jiakai Zhang <jiakaiz@google.com>
2022-09-19 16:06:26 +0000

    msm8998-common: Add a system property for System UI compiler filter
    Bug: 227310505
    Test: m
    Change-Id: Ieaa417a5024fd367543ff9879e5d57f0eb41aa94

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -161,7 +161,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Dex2oat
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.dex2oat64.enabled=true
+    dalvik.vm.dex2oat64.enabled=true \
+    dalvik.vm.systemuicompilerfilter=speed
 
 # Dexopt
 PRODUCT_PROPERTY_OVERRIDES += \
