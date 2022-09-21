0ff947340e581495bae123d809b1a73464bb0572
Anay Wadhera <anay1018@gmail.com>
2022-09-21 02:31:22 +0000

    msm8998-common: add bluetooth audio impl
    Android 13 requires android.hardware.bluetooth.audio-impl for audio init.
    
    Change-Id: Ibde0fd250c604c518a535c0f7e9b9000e45e4db4
    Reviewed-on: https://review.statixos.com/c/android_device_essential_mata/+/8305
    Reviewed-by: Anay Wadhera <anay1018@gmail.com>
    Tested-by: Anay Wadhera <anay1018@gmail.com>

--- msm8998.mk
+++ msm8998.mk
@@ -107,6 +107,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
+    android.hardware.bluetooth.audio-impl \
     android.hardware.bluetooth.audio@2.0-impl \
     libbthost_if
 
