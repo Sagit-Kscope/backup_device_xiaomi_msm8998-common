60af73fd88fccbd5ae62ffe0b59507a192701494
XsaviXander <22182013+XsaviXander@users.noreply.github.com>
2022-09-30 09:46:28 +0000

    msm8998-common: overlay: Enable freeform windows

--- msm8998.mk
+++ msm8998.mk
@@ -232,6 +232,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fs_config_files
 
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+    
 # Fwk-detect
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect \
