b153778dde1788b2594d7339980c3d91ae44ff45
Nanhumly <1607616422@qq.com>
2022-11-06 10:17:31 +0000

    msm8998-common: No need photos
    Change-Id: I6af1406aaed84cb90f12691f2addb0039df359ff

--- msm8998.mk
+++ msm8998.mk
@@ -501,9 +501,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
-# Photos
-include vendor/xiaomi/msm8998-common-extra/Photos.mk
-
 # Inherit several Android Go Configurations(Beneficial for everyone, even on non-Go devices)
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
