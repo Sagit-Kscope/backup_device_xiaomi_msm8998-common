469ad05c01419066a6daea2469ea1bbb9a0dede4
Huexxx <huexxx@gmail.com>
2022-11-22 12:22:44 +0100

    msm8998-common: Update Display stack from `LA.UM.10.6.2.r1-01600-89xx.0`
    - libgralloc.qti.so and libqdMetaData.so are included on repo but not copied atm
    
    Change-Id: Icdb146cd19cf69d7ae92ed13cd3bde81dc3804d3

--- msm8998.mk
+++ msm8998.mk
@@ -190,23 +190,21 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     gralloc.msm8998 \
     hwcomposer.msm8998 \
-    memtrack.msm8998 \
-    libdisplayconfig \
-    libdisplayconfig.qti.vendor \
     libqdMetaData \
     libqdMetaData.system \
-    libqdMetaData.vendor \
-    libtinyxml
+    libtinyxml \
+    libvulkan \
+    memtrack.msm8998 \
+    vendor.display.config@1.0.vendor \
+    vendor.qti.hardware.display.mapper@2.0.vendor
 
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.composer@2.1-impl \
-    android.hardware.graphics.composer@2.1-service \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.2-service \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
     android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
-    android.hardware.renderscript@1.0-impl
+    android.hardware.memtrack@1.0-service
 
 # Doze
 PRODUCT_PACKAGES += \
