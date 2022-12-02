4dd17221bcc7082328a516a3653394b3cfca8510
Huexxx <huexxx@gmail.com>
2022-12-02 14:00:58 +0000

    msm8998-common: Run ipa startup asynchronously
    Change-Id: If23acdd4aa92d3a684b047e0babf478029118a92

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -76,6 +76,14 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.ipastart.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_SRC_FILES    := bin/init.qcom.ipastart.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.post_boot.sh
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := EXECUTABLES
